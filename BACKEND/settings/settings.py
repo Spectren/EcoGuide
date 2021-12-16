faa6cc611688f4afcd8215cde46fccda9e0ebc58
Ivan Korchagin <ivank_ri@mail.ru>
2021-12-17 01:13:07 +0500

    v0.6 Clear admin

--- BACKEND/settings/settings.py
+++ BACKEND/settings/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ite28yhvcm4364ii5m0sms&5r*^#(rvfe3nfbu*#-n-vg3+c^0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
